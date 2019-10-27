ee45ed82b9049ea1f2fc5ac00601d18e3fbcef9a
Vladimir Medvedev <V.Megvegb@yandex.ru>
2019-10-27 22:46:33 +0300

    Creation new object JPanel

--- Games/src/CatchTheDrop.java
+++ Games/src/CatchTheDrop.java
@@ -1,17 +1,25 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class CatchTheDrop extends JFrame {
 //    private static GameWindows game_window;
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         JFrame gameWindows = new JFrame("Catch the Drop");
 
 //        game_window = new GameWindows();
         gameWindows.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        gameWindows.setLocation(200,100);
-        gameWindows.setSize(906,478);
+        gameWindows.setLocation(200, 100);
+        gameWindows.setSize(906, 478);
         gameWindows.setResizable(false);
         gameWindows.setVisible(true);
+    }
+
+    private static void onRepaint(Graphics g) {
+
+    }
+
+    private static class GameField extends JPanel {
 
     }
 }
